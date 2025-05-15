282c5c446a9dca116455ed5bff91cdd1a1508ff9
sneshev <steffal.n2@gmail.com>
2025-05-15 20:31:36 +0200

    window closes when you click the x

--- src/event_handle_mlx.c
+++ src/event_handle_mlx.c
@@ -106,8 +106,15 @@ int	set_keyhooks(int key, t_data *data)
 	return (1);
 }
 
+int close_window(t_data *data)
+{
+	free_data(data);
+	exit(0);
+}
+
 void	mlx_event_handle(t_data *data)
 {
 	mlx_hook(data->win, 4, 4, set_mouse_events, data);
+	mlx_hook(data->win, 17, 0, close_window, data);
 	mlx_key_hook(data->win, set_keyhooks, data);
 }
